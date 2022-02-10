5c6e70961e88e2283d09a8b78c75dc989bf68447
ibrahmozdemir1 <46843192+ibrahmozdemir1@users.noreply.github.com>
2022-02-10 03:11:12 +0300

    Tüm işlemler tamamlandı.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'package:flutter/material.dart';
-import 'userpage.dart';
+import 'package:listandsearch_user/pages/userpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,18 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
       home: Jobs(),
     );
